71c3237858f5bda71c07b28d21e64a4d292eadc0
Hersh Nagpal <hershnagpal@Hershs-MacBook-Pro.local>
2020-06-21 03:25:54 -0400

    added crt effect

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Icon from '../src/components/Icon'
 import Taskbar from './components/Taskbar'
 import './styles/style.css';
+import './styles/crt.css';
 
 import TextFileIcon from './assets/images/icons/txt.png';
 import DocumentFileIcon from './assets/images/icons/doc.png';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App crt">
       <Icon iconImage={TextFileIcon} iconText={"textxtxtxtxtxtxt.txt"} />
       <Icon iconImage={DocumentFileIcon} iconText={"text.pdf"} />
       <Taskbar/>
