55fa5004c85d92a738e3fb0d7bfe24579daa0fd7
Hersh Nagpal <hershnagpal@gmail.com>
2020-06-24 04:02:30 -0400

    windows are draggable and appear when task is open

--- src/components/Window.js
+++ src/components/Window.js
@@ -2,38 +2,52 @@ import React from 'react'
 import '../styles/window.css';
 import WindowButton from './WindowButton';
 import MenuBar from './MenuBar';
-import {iconData, findImageSource} from '../iconData' 
+import {isWindowOpen} from '../taskbarState'
+import {iconData, findImageSource} from '../iconData'
+import Draggable from 'react-draggable';  
 
 export const Window = ({taskbarState, updateTaskbar, id}) => {
-
-    const thisWindowsTask = taskbarState.find((task) => task.id === id)
-
-    if (!thisWindowsTask.windowIsOpen) {
-        return null
-    }
+    if (!isWindowOpen(id, taskbarState)) return null
 
     return (
-        <div 
-            className="window"
-        >
-            <div className="row">
-                <div className="column left">
-                    <p className="title">{iconData[id].title}</p>
-                </div>
-                <div className="column middle">
-                    <p/>
-                </div>
-                <div className="column right">
-                    <div className="buttons" style={{float: "right"}}>
-                    <WindowButton type={"minimize"}/>
-                    <WindowButton type={"maximize"}/>
-                    <WindowButton type={"close"}/>
+        <Draggable>
+            <div 
+                className="window"
+            >
+                <div className="row">
+                    <div className="column left">
+                        <p className="title">{iconData[id].title}</p>
+                    </div>
+                    <div className="column middle">
+                        <p/>
+                    </div>
+                    <div className="column right">
+                        <div className="buttons">
+                        <WindowButton 
+                            type={"minimize"} 
+                            taskbarState={taskbarState} 
+                            updateTaskbar={updateTaskbar} 
+                            id={id}
+                        />
+                        <WindowButton 
+                            type={"maximize"} 
+                            taskbarState={taskbarState} 
+                            updateTaskbar={updateTaskbar} 
+                            id={id}
+                        />
+                        <WindowButton 
+                            type={"close"} 
+                            taskbarState={taskbarState} 
+                            updateTaskbar={updateTaskbar} 
+                            id={id}
+                        />
+                        </div>
                     </div>
                 </div>
+                <div className="">
+                    <MenuBar className="menuBar" type={"txt"}/>
+                </div>
             </div>
-            <div className="">
-                <MenuBar className="menuBar" type={"txt"}/>
-            </div>
-        </div>
+        </Draggable>
     )
 }
