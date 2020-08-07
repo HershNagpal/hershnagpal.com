abc797d52a9e9b479ec018860eca298c0ba65bea
Hersh <hershnagpal.business@gmail.com>
2020-08-07 19:16:10 -0400

    Gallbladder message

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,19 @@ import "../styles/content.css"
 export const AboutMe = () => {
     return (
         <div className="text">
-            <h3>Hersh</h3>
+            <h3>Hersh Nagpal</h3>
+            <p>
+                The engineer is the gallbladder of their team. No one really understands it's <br/>
+                function. It's a humble and hardworking organ that's not very glamorous to <br/>
+                think about. You could probably live without it but it makes your life easier <br/>
+                behind the scenes. <br/>
+            </p>
+            <p>
+                I'm an artist and software engineering student. I have been a gallbladder for <br/>
+                a few years now and I love to bring my hobbies and interests into my field. <br/>
+                This website is my way of sharing my combination of nostalgic art style and <br/>
+                love for engineering with you.<br/>
+            </p>
         </div>
     )
 }
