05ff89baeebaa469e01ef2f8646e9c8c69212798
Hersh Nagpal <hershnagpal@gmail.com>
2021-03-30 13:02:23 -0400

    Small updates to animations and windows

--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -3,6 +3,11 @@ import {StartMenuItem} from './StartMenuItem'
 import '../styles/startMenu.css'
 
 export const StartMenu = () => {
+
+    const shutDown = () => {
+        console.log("Shut Down")
+    }
+
     return (
         <div id="startMenu">
             <div id="left">
@@ -11,12 +16,12 @@ export const StartMenu = () => {
                     <span className="num"> 98</span>
                 </div>
             </div>
-            <StartMenuItem text={"Documents"}/>
-            <StartMenuItem text={"Photos"}/>
-            <StartMenuItem text={"Contact"}/>
-            <StartMenuItem text={"Find"}/>
-            <StartMenuItem text={"Help"}/>
-            <StartMenuItem text={"Shut Down"}/>
+            <StartMenuItem text={"Documents"} />
+            <StartMenuItem text={"Photos"} />
+            <StartMenuItem text={"Contact"} />
+            <StartMenuItem text={"Find"} />
+            <StartMenuItem text={"Help"} />
+            <StartMenuItem text={"Shut Down"} handleClick={shutDown}/>
         </div>
     )
 }
--- src/components/StartMenuItem.js
+++ src/components/StartMenuItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../styles/startMenu.css'
 
-export const StartMenuItem = ({text}) => {
+export const StartMenuItem = ({text, handleClick}) => {
+    
     return (
-        <div className="menuItem">
+        <div className="menuItem" onClick={handleClick}>
             {text}
         </div>
     )
